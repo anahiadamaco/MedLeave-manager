4e000072764930855e594467d7608bb9009c0cce
Iss <111464133+IssPame@users.noreply.github.com>
2025-11-16 13:44:42 -0300

    .

--- MedLeaveExpo2/src/config/api.ts
+++ MedLeaveExpo2/src/config/api.ts
@@ -5,8 +5,7 @@
  * Si quieres cambiar a un servidor en la nube, solo cambia esta URL
  */
 
-// export const API_BASE_URL = "http://192.168.100.223:3000"; Esta es la original
-export const API_BASE_URL = "http://169.254.132.253:3000"; // esta la de mi PC
+export const API_BASE_URL = "http://172.16.5.49:3000";
 
 // Rutas de autenticación
 export const AUTH_ROUTES = {
